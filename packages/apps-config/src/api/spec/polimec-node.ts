ee5200dc3089ad0f183cd2b1d26f709b2ea44749
Albrecht <14820950+weichweich@users.noreply.github.com>
2020-07-24 10:34:27 +0200

    nice polimec node types

--- packages/apps-config/src/api/spec/polimec-node.ts
+++ packages/apps-config/src/api/spec/polimec-node.ts
@@ -1,14 +1,21 @@
+// Copyright 2017-2020 @polkadot/apps-config authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+// structs need to be in order
+/* eslint-disable sort-keys */
+
 export default {
   Address: 'AccountId',
-  BlockNumber: 'u32',
   Index: 'u32',
   LookupSource: 'AccountId',
+  BlockNumber: 'u32',
   Signature: 'MultiSignature',
+
+  Amount: 'i128',
+  AmountOf: 'i128',
   Balance: 'u128',
   BalanceOf: 'u128',
-
   CurrencyId: 'u32',
-  CurrencyIdOf: 'u32',
-  AmountOf: 'i128',
-  Amount: 'i128'
+  CurrencyIdOf: 'u32'
 };
