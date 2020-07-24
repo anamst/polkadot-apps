eb08f30fe8ff84db500e09a3abe697103a7490b8
Albrecht <14820950+weichweich@users.noreply.github.com>
2020-07-24 10:31:46 +0200

    fix polimec node types

--- packages/apps-config/src/api/spec/polimec-node.ts
+++ packages/apps-config/src/api/spec/polimec-node.ts
@@ -1,10 +1,14 @@
 export default {
   Address: 'AccountId',
-  BlockNumber: 'u64',
-  Index: 'u64',
+  BlockNumber: 'u32',
+  Index: 'u32',
   LookupSource: 'AccountId',
   Signature: 'MultiSignature',
+  Balance: 'u128',
+  BalanceOf: 'u128',
 
   CurrencyId: 'u32',
-  CurrencyIdOf: 'u32'
+  CurrencyIdOf: 'u32',
+  AmountOf: 'i128',
+  Amount: 'i128'
 };
