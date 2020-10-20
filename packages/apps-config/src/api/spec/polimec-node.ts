3c2ff8dab47921c879542a887837acffe8406e00
wischli <w.freude@icloud.com>
2020-10-20 13:29:24 +0200

    fix: add xtokens types

--- packages/apps-config/src/api/spec/polimec-node.ts
+++ packages/apps-config/src/api/spec/polimec-node.ts
@@ -19,9 +19,27 @@ export default {
   AmountOf: 'i128',
   Balance: 'u128',
   BalanceOf: 'u128',
+  // neither Vec<8u> nor Bytes nor String works
   CurrencyId: '[u8; 8]',
   CurrencyIdOf: 'CurrencyId',
+  ParaId: 'u32',
+  RelayChainBlockNumber: 'BlockNumber',
+  // required in 2.0.0
   RefCount: 'u32',
-  XCurrencyId: 'CurrencyId',
-  ParaId: 'u32'
+  // types for orml_xtokens
+  ChainId: {
+    _enum: {
+      RelayChain: 'Null',
+      ParaChain: 'ParaId'
+    }
+  },
+  XCurrencyId: {
+    chain_id: 'ChainId',
+    currency_id: 'CurrencyId'
+  },
+  XCMPTokenMessage: {
+    _enum: {
+      Transfer: '(XCurrencyId, ParaId, AccountId, Balance)'
+    }
+  }
 };
